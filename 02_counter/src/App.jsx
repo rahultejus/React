d8d28751ab96f826cf5b6801df5fa8059575cc0c
rahultejus <rahultejus123@gmail.com>
2025-01-12 17:31:25 +0530

    counter project completed

---
+++ 02_counter/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // let counter=15
+  let [counter,setCounter]=useState(10)
+
+  const addValue=()=>{
+    // counter+=1
+    if(counter<20)
+    setCounter(counter+1)
+    console.log("Value added",counter)
+  }
+  const removeValue=()=>{
+    if(counter!=0)
+    setCounter(counter-1)
+    console.log("Value removed",counter)
+  }
+
+  return (
+    <>
+      <h1>Value Increment and Decrement</h1>
+      <p>Current Value:{counter}</p>
+      <p>
+      <button
+      onClick={addValue}>Add Value</button>
+      </p>
+      <button
+      onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
