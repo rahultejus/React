4801a5a952a102afc6a799218d4a326484895877
rahultejus <rahultejus123@gmail.com>
2025-01-13 16:10:35 +0530

    use of props based project

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Cards from './components/Cards'
+function App() {
+
+  return (
+    <>
+      <Cards  title="Boost your react knowledge" start="Start Your Journey"/>
+      <Cards title="Boost your Tailwind CSS knowledge" start="Start with new way"/>
+    </>
+  )
+}
+
+export default App
